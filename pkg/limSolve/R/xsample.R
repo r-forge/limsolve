39373b542bac96b9988c52092756c851145ec172
karlines <karlines@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-09-02 06:37:35 +0000

    GPL (>= 2); matrix creation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/limsolve@69 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/limSolve/R/xsample.R
+++ pkg/limSolve/R/xsample.R
@@ -255,14 +255,14 @@ xsample <- function(A=NULL, B=NULL, E=NULL, F=NULL, G=NULL, H=NULL,
   ou <- ceiling(iter/outputlength)
 
   q1 <- rep(0,k)
-  x <- matrix(ncol=lx,nrow=outputlength,dimnames=list(NULL,colnames(A)))
+  x <- matrix(nrow=outputlength,ncol=lx,dimnames=list(NULL,colnames(A)))
   x[1,] <- x0
   naccepted <- 1
   p <- vector(length=outputlength) # probability distribution
   p[1] <- prob(q1)
 
   if (fulloutput)   {
-    q <- matrix(ncol=k,nrow=outputlength)
+    q <- matrix(nrow=outputlength, ncol=k)
     q[1,] <- q1
   }
     
